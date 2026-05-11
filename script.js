63eaf6932d45a2056e09cc017007338988f395b9
marisystems <215432766+marisystems@users.noreply.github.com>
2026-05-11 17:29:14 -0300

    Finish Implementing style swtiching
    It's a lot of moving parts but it looks pretty good!

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 function setTheme() {
-  const root = document.documentElement;
-  const newTheme = root.className === 'light' ? 'dark' : 'light';
-  root.className = newTheme;
+    const root = document.documentElement;
+    const newTheme = root.className === 'light' ? 'dark' : 'light';
+    root.className = newTheme;
+}
+
+function changeIcon() {
+    const icon = document.querySelector(".theme-toggle img");
+    if (icon.getAttribute('src') === './img/icon-light.svg') {
+        icon.setAttribute('src', './img/icon-dark.svg')
+    } else {
+        icon.setAttribute('src', './img/icon-light.svg')
+    }
 }
 
 setTheme();
+
 document.querySelector('.theme-toggle').addEventListener('click', () => {
-    setTheme()
-    console.log("Clicked")
+    setTheme();
+    changeIcon();
 })
 
